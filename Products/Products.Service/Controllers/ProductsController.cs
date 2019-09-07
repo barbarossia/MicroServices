b0d65089c4ed1dd5e0354bab360ba022db9f2983
Aless Wing <barbarossia@Alesss-MacBook.local>
2019-09-07 15:22:16 +0800

    initial commit

---
+++ Products/Products.Service/Controllers/ProductsController.cs
@@ -0,0 +1,91 @@
+using System;
+using System.Web.Http;
+using System.Net;
+using System.Net.Http;
+using Products.Service.DataTransferObjects.Commands;
+using Products.Service.MicroServices.Products.Commands;
+using Products.Service.MicroServices.Products.Handlers;
+using MicroServices.Common.Exceptions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Products.Service.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly ProductCommandHandlers handler;
+
+        public ProductsController()
+            : this(ServiceLocator.ProductCommands)
+        {}
+
+        public ProductsController(ProductCommandHandlers handler)
+        {
+            this.handler = handler;
+        }
+
+        [HttpPost]
+        public ActionResult Post(CreateProductCommand cmd)
+        {
+            if (string.IsNullOrWhiteSpace(cmd.Name))
+            {
+                var response = new HttpResponseMessage(HttpStatusCode.Forbidden)
+                {
+                    Content = new StringContent("code must be supplied in the body"),
+                    ReasonPhrase = "Missing product code"
+                };
+                //throw new HttpResponseException(response);
+                
+            }
+
+            try
+            {
+                var command = new CreateProduct(Guid.NewGuid(), cmd.Name, cmd.Description, cmd.Price);
+                handler.Handle(command);
+
+                var link = new Uri(string.Format("http://localhost:8181/api/products/{0}", command.Id));
+                //return Created<CreateProduct>(link, command);
+                return null;
+            }
+            catch (AggregateNotFoundException)
+            {
+                return NotFound();
+            }
+            catch (AggregateDeletedException)
+            {
+                return Conflict();
+            }
+        }
+
+        [HttpPut("api/products/{id:guid}")]
+        public ActionResult Put(Guid id, AlterProductCommand cmd)
+        {
+            if (string.IsNullOrWhiteSpace(cmd.Name))
+            {
+                var response = new HttpResponseMessage(HttpStatusCode.Forbidden)
+                {
+                    Content = new StringContent("code must be supplied in the body"),
+                    ReasonPhrase = "Missing product code"
+                };
+                //throw new HttpResponseException(response);
+            }
+
+            try
+            {
+                var command = new AlterProduct(id, cmd.Version, cmd.Name, cmd.Description, cmd.Price);
+                handler.Handle(command);
+
+                return Ok(command);
+            }
+            catch (AggregateNotFoundException)
+            {
+                return NotFound();
+            }
+            catch (AggregateDeletedException)
+            {
+                return Conflict();
+            }
+        }
+    }
+}
